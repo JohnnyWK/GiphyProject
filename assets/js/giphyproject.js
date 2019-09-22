99eaf6ae95b9f3aaf864717de709ac90e9f1e63d
Johnny Kreitzman <kreitzka@gmail.com>
2019-09-22 11:27:21 -0500

    image area populating without giphy image)

--- assets/js/giphyproject.js
+++ assets/js/giphyproject.js
@@ -20,18 +20,18 @@ function renderButtons() {
 }
 
 function newButton() {
-    $("#add-gifs").on("click", function () {
+    $("#add-gifs").on("click", function (event) {
+        event.preventDefault();
         var gif = $("#giphy-input").val().trim();
-        topic.push(gif);
-
+        gifs.push(gif);
         renderButtons();
     });
 }
 
 
-function getGifs(gif) {
+function getGifs() {
     var gif = $(this).attr("data-name");
-    var queryURL = "api.giphy.com/v1/gifs/search?q=" + gif + "&apikey=YAVt4z2t61wPd1oqatWrOZlv0pgRNXke=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=YAVt4z2t61wPd1oqatWrOZlv0pgRNXke";
 
     $.ajax({
         url: queryURL,
@@ -40,23 +40,24 @@ function getGifs(gif) {
         .then(function (response) {
             $("#giphyArea")
             var results = response.data;
-
+           
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 gifDiv.attr("class", "gifsDiv");
                 var ratingP = $("<p>");
                 ratingP.text("Rating:" + results[i].rating);
                 var image = $("<img>");
+                image.attr(results[i].images.preview);
                 gifDiv.append(ratingP);
                 gifDiv.append(image);
                 $("#giphyArea").prepend(gifDiv);
             }
-
+            console.log(results)
         })
 }
 
 renderButtons();
 newButton();
 
-$(document).on("click", ".gif", getGifs)
+$(document).on("click", ".giphy", getGifs)
   
